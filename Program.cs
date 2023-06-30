f13ceaa1f49990660029e04d99c19c9d34290979
F R <raufi4u@gmail.com>
2023-07-01 00:45:21 +0200

    [update]

--- Program.cs
+++ Program.cs
@@ -11,12 +11,22 @@ class Program
         //Encapsulation encapsulation = new Encapsulation();
         //encapsulation.BankHidingDataMethod();
 
-        PolymorphismMethodOverloading polymorphismMethodOverloading = new PolymorphismMethodOverloading();
+        //PolymorphismMethodOverloading polymorphismMethodOverloading = new PolymorphismMethodOverloading();
         //polymorphismMethodOverloading.OverloadingMethod();
         //polymorphismMethodOverloading.InheritanceBasedMethodOverloadingMethod();
         //polymorphismMethodOverloading.ConstructorOverloadingMethod();
         //polymorphismMethodOverloading.RealtimeOverloadingMethod();
+
+        //PolymorphismOperatorOverloading polymorphismOperatorOverloading = new PolymorphismOperatorOverloading();
+
+        //PolymorphismMethodOverriding polymorphismMethodOverriding = new PolymorphismMethodOverriding();
+
+        // MethodOverridingRealTimeExample methodOverridingRealTimeExample = new MethodOverridingRealTimeExample();
+
+        PolymorphismMethodHiding polymorphismMethodHiding = new PolymorphismMethodHiding();
     }
 }
 
 //https://dotnettutorials.net/lesson/object-oriented-programming-csharp/
+//https://dotnettutorials.net/lesson/class-and-objects-csharp/
+//https://dotnettutorials.net/lesson/access-specifiers-csharp/
